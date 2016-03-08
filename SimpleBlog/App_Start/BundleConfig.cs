500909fd5333a4bc679005cac23c052be04e7039
Fidelis Efomah <efomahfidelis@gmail.com>
2016-03-08 01:19:01 +0100

    Imported styles and scripts (jQuery and Bootstrap)

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Site.css"));
+
+            bundles.Add(new StyleBundle("~/admin/scripts")
+                .Include("~/Scripts/modernizr-1.7.js")
+                .Include("~/Scripts/jquery-2.2.1.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/ie10-viewport-bug-workaround.js")
+                .Include("~/Scripts/ie-emulation-modes-warning.js")
+                .Include("~/Scripts/holder.js"));
+
+            //bundles.Add(new StyleBundle("~/scripts")
+            //    .Include("~/Scripts/modernizr-1.7.js")
+            //    .Include("~/Scripts/jquery-2.2.1.js")
+            //    .Include("~/Scripts/jquery.validate.js")
+            //    .Include("~/Scripts/jquery.validate.unobtrusive.js")
+            //    .Include("~/Scripts/bootstrap.js")
+            //    .Include("~/Scripts/ie10-viewport-bug-workaround.js")
+            //    .Include("~/Scripts/ie-emulation-modes-warning.js")
+            //    .Include("~/Scripts/holder.js"));
+        }
+    }
+}
