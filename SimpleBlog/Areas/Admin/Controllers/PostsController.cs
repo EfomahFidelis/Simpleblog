b89b529d90d2ddc613d29c4f953a8a3eb8cf38bf
Fidelis Efomah <efomahfidelis@gmail.com>
2016-05-06 10:15:36 +0100

    Created new and actions for the post controller

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -14,7 +14,7 @@ namespace SimpleBlog.Areas.Admin.Controllers
      [SelectedTab("posts")]
     public class PostsController: Controller
     {
-         private const int PostPerPage = 5;
+         private const int PostPerPage = 40;
 
 
         public ActionResult Index(int page = 1)
@@ -34,6 +34,67 @@ namespace SimpleBlog.Areas.Admin.Controllers
             { 
                 Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostPerPage)
             });
+        }
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm 
+            { 
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        { 
+            var post = Database.Session.Load<Post>(id);
+                if(post == null)
+                    return HttpNotFound();
+            return View("Form", new PostsForm
+            {
+                 IsNew = false,
+                PostId = id,
+                Content = post.Content,
+                Slug = post.Slug,
+                Title = post.Title,
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == null;
+
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    User = Auth.User,
+                };
+            }
+            else {
+
+                post = Database.Session.Load<Post>(form.PostId);
+
+                if (post == null)
+                    return HttpNotFound();
+
+                post.UpdatedAt = DateTime.UtcNow;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using SimpleBlog.Infrastructure;
 using SimpleBlog.Models;
+using System.ComponentModel.DataAnnotations;
 
 
 
@@ -12,5 +13,20 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, StringLength(128)]
+        public string Title { get; set; }
+
+        [Required, StringLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
